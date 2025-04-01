aebaa1e0bcda1f9ab8af4206ab3f39123ce655ac
Shyam_0373 <shyamtripathi373@gmail.com>
2025-04-01 15:23:29 +0530

    Global.css modified
    tailwind.config.js also modified to remove error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
